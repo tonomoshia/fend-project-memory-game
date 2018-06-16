7610e2530c8770ca9ddc835058b303d7f859e648
Tonomoshia <tonomoshia@gmail.com>
2018-06-16 15:26:26 -0400

    Flip cards and add open and show classes to them

--- js/app.js
+++ js/app.js
@@ -12,7 +12,8 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -24,6 +25,15 @@ function shuffle(array) {
 
     return array;
 }
+//Flip cards and add open and show classes to them
+var allCards = document.querySelectorAll('.card');
+
+allCards.forEach(function (card) {
+    card.addEventListener('click', function (e) {
+        card.classList.add('open', 'show');
+    });
+});
+
 // TODO: add Event Listener
 function createCard(className) {
 
